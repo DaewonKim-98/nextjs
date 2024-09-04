75f5d64303a81c086a2abc60c9b8de994cfbcc7f
daewon <harry5414@gmail.com>
2024-09-04 21:12:51 +0900

    [Init] NextMovie

---
+++ components/movie-info.tsx
@@ -0,0 +1,22 @@
+import { API_URL } from "../app/(home)/page"
+import style from "../styles/movie-info.module.css"
+
+export async function getMovie(id:string) {
+  const response = await fetch(`${API_URL}/${id}`)
+  return response.json()
+}
+
+export default async function MovieInfo({id}:{id:string}) {
+  const movie = await getMovie(id)
+  return (
+    <div className={style.container}>
+      <img src={movie.poster_path} alt="" />
+      <div>
+        <h1>{movie.title}</h1>
+        <h3>✨{movie.vote_average.toFixed(1)}</h3>
+        <p>{movie.overview}</p>
+        <a href={movie.homepage} target={"_blank"}>Homepage &rarr;</a>
+      </div>
+    </div>
+  )
+}
---
+++ components/navigation.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import styles from  "../styles/navigation.module.css"
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function Navigation() {
+	const path = usePathname()
+	return <nav className={styles.nav}>
+		<ul>
+			<li>
+				<Link href="/">Home</Link> {path === '/' ? '😁' : ''}
+			</li>
+			<li>
+				<Link href="/about-us">About Us</Link> {path === '/about-us' ? '😁' : ''}
+			</li>
+		</ul>
+	</nav>
+}
